a0ae785bf7f3cef46c375417f3b0bab89ecf4449
Jean Wasilewski <henyxia@live.fr>
2015-04-16 15:16:18 +0000

    Non working version

--- main.c
+++ main.c
@@ -1,49 +1,36 @@
-/* 
-	Remember to compile try:
-		1) gcc hi.c -o hi -lX11
-		2) gcc hi.c -I /usr/include/X11 -L /usr/X11/lib -lX11
-		3) gcc hi.c -I /where/ever -L /who/knows/where -l X11
-
-	Brian Hammond 2/9/96.    Feel free to do with this as you will!
-*/
-
-
-/* include the X library headers */
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
-
-/* include some silly stuff */
 #include <stdio.h>
 #include <stdlib.h>
 
-/* here are our X variables */
+#define AWESOME 7
+
 Display *dis;
 int screen;
 Window win;
 GC gc;
 int width=320;
 int height=240;
+int i=0;
 
-/* here are our X routines declared! */
 void init_x();
 void close_x();
 void redraw();
 
-main () {
+int main ()
+{
 	XEvent event;		/* the XEvent declaration !!! */
 	KeySym key;		/* a dealie-bob to handle KeyPress Events */	
 	char text[255];		/* a char buffer for KeyPress Events */
-	Pixmap pmap;
 	XColor col;
+	int initOffsetBMP;
 
 	init_x();
 
 	printf("Patte de canard\n");
 	Colormap cmap = DefaultColormap(dis, DefaultScreen(dis));
 	printf("?\n");
-	pmap = XCreatePixmap(dis, win, width, height, DefaultDepth(dis, DefaultScreen(dis)));
-
 
 	FILE* home = NULL;
 	char buffer[4];
@@ -57,6 +44,7 @@ main () {
 	buffer[2] = getc(home);
 	buffer[3] = getc(home);
 	printf("Pixel array offset %02x %02x %02x %02x\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+	initOffsetBMP = buffer[0] + (buffer[1] >> 8) + (buffer[2] >> 16) + (buffer[3] >> 24);
 
 	fseek(home, 0x12, SEEK_SET);
         buffer[0] = getc(home);
@@ -84,61 +72,72 @@ main () {
 	}
 
 
+	fseek(home, initOffsetBMP, SEEK_SET);
 
-
-	while(1);
-
-
-	/* look for events forever... */
-	while(1) {		
-		/* get the next event and stuff it into our event variable.
-		   Note:  only events we set the mask for are detected!
-		*/
+	while(1)
+	{
 		XNextEvent(dis, &event);
 	
-		if (event.type==Expose && event.xexpose.count==0) {
-		/* the window was exposed redraw it! */
+		if (event.type==Expose && event.xexpose.count==0)
 			redraw();
-		}
-		if (event.type==KeyPress&&
-		    XLookupString(&event.xkey,text,255,&key,0)==1) {
-		/* use the XLookupString routine to convert the invent
-		   KeyPress data into regular text.  Weird but necessary...
-		*/
-			if (text[0]=='q') {
+
+		if (event.type==KeyPress&& XLookupString(&event.xkey,text,255,&key,0)==1)
+		{
+			if (text[0]=='q')
 				close_x();
-			}
 			printf("You pressed the %c key!\n",text[0]);
 		}
-		if (event.type==ButtonPress) {
-		/* tell where the mouse Button was Pressed */
-			int x=event.xbutton.x,
-			    y=event.xbutton.y;
+		if (event.type==ButtonPress)
+		{
+			//int x=event.xbutton.x,
+			    //y=event.xbutton.y;
 
 			//strcpy(text,"X is FUN!");
 			//XSetForeground(dis,gc,rand()%event.xbutton.x%255);
 			//XDrawString(dis,win,gc,x,y, text, strlen(text));
 
+		        buffer[0] = getc(home);
+		        buffer[1] = getc(home);
+		        buffer[2] = getc(home);
 
-			col.red = 128;
-			col.green = 128;
-			col.blue = 0;
-			XAllocColor(dis, cmap, &col);
-			XSetForeground(dis, gc, col.pixel);
-			XDrawPoint(dis,win,gc,x,y);
+			int j;
+
+			for(j=0; j<AWESOME; j++)
+			{
+				col.red = buffer[2];
+				col.green = buffer[1];
+				col.blue = buffer[0];
+
+
+
+
+
+				if(XAllocColor(dis, cmap, &col) == 0)
+					printf("Failed to allocate color\n");
+				if(XSetForeground(dis, gc, col.pixel) == 0)
+					printf("Failed to set foreground\n");
+				if(XDrawPoint(dis,win,gc,i%width,i/height) == 0)
+					printf("Failed to draw point\n");
+			        buffer[0] = getc(home);
+			        buffer[1] = getc(home);
+			        buffer[2] = getc(home);
+				printf("X %03d Y %03d R %02x G %02x B %02x\n", i%width, i/height, buffer[0], buffer[1], buffer[2]);
+				i++;
+			}
 		}
 	}
+	printf("End of display\n");
+	return 0;
 }
 
 void init_x() {
-/* get the colors black and white (see section for details) */        
 	unsigned long black,white;
 
 	dis=XOpenDisplay((char *)0);
    	screen=DefaultScreen(dis);
 	black=BlackPixel(dis,screen),
 	white=WhitePixel(dis, screen);
-   	win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,height,width, 0,black, white);
+   	win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,width,height, 0,black, white);
 	XSetStandardProperties(dis,win,"Howdy","Hi",None,NULL,0,NULL);
 	XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
         gc=XCreateGC(dis, win, 0,0);        
